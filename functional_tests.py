bbf551a5e96e24ca387362877b000195d4a3d36a
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-05-08 09:36:29 +0200

    functional_tests now uses unittest.TestCase

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,43 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Congratulations' in browser.title
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+    
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Jane heart about a cool new website to-do app.
+        # So she goes to homepage to check it
+        self.browser.get('http://localhost:8000')
+
+        # She notices that title says to-do
+        self.assertIn('To-Do', self.browser.title)
+        self.fail()
+
+        # She is invited to enter a to-do item 
+
+        # She types '1: do ironing'
+
+        # Then after she pressed enter the page refreshes and new item appears at it
+
+        # There is still textbox inviting her to type
+
+        # She types '2: do homework'
+
+        # After she press enter page reloads and both items are shown
+
+        # Jane wonders wheather site will remember her list. 
+        # She sees that site has generated a unique URL for her
+
+        # She visites that URL and sees her to-do list
+
+        # Satisfied she goes sleep
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
 
