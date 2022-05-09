495ab48e7802dbe6a0cde4eb79ecefe3eeb34838
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-05-09 07:59:02 +0200

    Refactor homepage view to use templates

--- lists/tests.py
+++ lists/tests.py
@@ -1,20 +1,11 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
 from lists.views import homepage
 
 
 class HomePageTests(TestCase):
 
-    def test_root_url_resolves_homepage_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, homepage)
-
-    def test_homepage_returns_correct_html(self):
-        request = HttpRequest()
-        response = homepage(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do list</title>', html)
-        self.assertTrue(html.endswith('</html>'))
+    def test_uses_home_template(self):
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def homepage(request):
-    return HttpResponse('<html><title>To-Do list</title></html>')
-
+    return render(request, 'home.html')
