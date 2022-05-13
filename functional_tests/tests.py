af6e0dad19a4863762d79a5d0628ef632d562989
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-05-13 14:23:30 +0200

    make functional_tests app, use LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,10 +1,10 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import unittest
 import time
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -20,7 +20,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Jane heart about a cool new website to-do app.
         # So she goes to homepage to check it
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
         # She notices that title and headline says to-do
         self.assertIn('To-Do', self.browser.title)
@@ -60,7 +60,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # She visites that URL and sees her to-do list
         # Satisfied she goes sleep
-
-
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
