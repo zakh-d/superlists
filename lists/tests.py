2f9c7bbc1d158c7e8898ae88b89618e21596f18f
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-05-13 13:48:42 +0200

    Redirect after POST, and show all items in templates

--- lists/tests.py
+++ lists/tests.py
@@ -1,4 +1,5 @@
 from django.test import TestCase
+from django.urls import reverse
 from lists.models import Item
 
 
@@ -9,9 +10,28 @@ class HomePageTests(TestCase):
         self.assertTemplateUsed(response, 'home.html')
 
     def test_can_save_a_POST_request(self):
+        self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, 'A new list item')
+
+
+    def test_redirects_after_POST(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
-        self.assertContains(response, 'A new list item')
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertRedirects(response, reverse('homepage'))
+
+    def test_only_save_item_when_it_is_necessary(self):
+        self.client.get('/')
+        self.assertEqual(Item.objects.count(), 0)
+
+    def test_displays_all_list_items(self):
+        Item.objects.create(text='first item')
+        Item.objects.create(text='second item')
+
+        response = self.client.get('/')
+        self.assertContains(response, 'first item')
+        self.assertContains(response, 'second item')
+
 
 class ItemModelTests(TestCase):
 
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from lists.models import Item
 
 
 def homepage(request):
-    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
+
+    if request.method == 'POST':
+        new_item_text = request.POST['item_text']
+        Item.objects.create(text=new_item_text)
+        return redirect(reverse('homepage'))
+    
+    items = Item.objects.all()
+    return render(request, 'home.html', {'items': items})
