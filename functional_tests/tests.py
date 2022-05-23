c3c27eff707e587c6d89ed1ec9f98804186c65b9
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-05-23 11:17:17 +0300

    new func tests fails

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,10 @@
+import time
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import time
+from selenium.common.exceptions import WebDriverException
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -12,12 +15,20 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as error:
+                if time.time() - start_time > MAX_WAIT:
+                    raise error
+                time.sleep(.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # Jane heart about a cool new website to-do app.
         # So she goes to homepage to check it
         self.browser.get(self.live_server_url)
@@ -41,7 +52,7 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
 
         # Then after she pressed enter the page refreshes and new item appears at it
-        self.check_for_row_in_list_table('1: Do ironing')
+        self.wait_for_row_in_list_table('1: Do ironing')
 
         # There is still textbox inviting her to type
         # She types 'Do homework'
@@ -51,12 +62,45 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(1)
 
         # After she press enter page reloads and both items are shown
-        
-        self.check_for_row_in_list_table('2: Do homework')
+        self.wait_for_row_in_list_table('1: Do ironing')
+        self.wait_for_row_in_list_table('2: Do homework')
 
-        # Jane wonders wheather site will remember her list.
+    def test_can_start_list_for_multiple_users(self):
+        # Jane starts new todo list
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy peacock feathers!')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers!')
         # She sees that site has generated a unique URL for her
-        self.fail('Finish tests')
+        jane_list_url = self.browser.current_url
+        self.assertRegex(jane_list_url, r'/lists/.+')
 
-        # She visites that URL and sees her to-do list
-        # Satisfied she goes sleep
+        # Now a new user, Francis, comes along to the site
+        
+        ## We use a new browser session to make sure no info
+        ## about Jane come from cookies
+
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis visits homepage and there is no sign of Jane's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers!', page_text)
+        self.assertNotIn('Do homework', page_text)
+        # Francis start a new list by entering a new item
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('Buy milk')
+
+        # Francis gets his own url
+        francis_list_url = self.browser.current_url
+        self.assertNotEqual(francis_list_url, jane_list_url)
+        self.assertRegex(francis_list_url, '/lists/.+')
+
+        # Again there is no trace of Jane's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers!', page_text)
+        self.assertIn('Buy milk', page_text)
