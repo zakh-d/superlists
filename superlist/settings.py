06e3252975dcec5f2428220413a2546c118f4d1f
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-06-06 14:39:44 +0200

    changed allowed hosts

--- superlist/settings.py
+++ superlist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!p-nh!hw(7h%x^evct-=q^ichueu084za+o3^qvtia(wacuwel
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:8000', 'http://localhost:8000', 'http://dev.superlist.site']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'dev.superlist.site']
 
 
 # Application definition
