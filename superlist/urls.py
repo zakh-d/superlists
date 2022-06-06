9bbb98850ba8715facfcd8f1bf9d7e067772c99c
danylozakharchenko <danylo.zakharcehnko@gmail.com>
2022-06-06 08:06:30 +0200

    relocated links urls to its own file

--- superlist/urls.py
+++ superlist/urls.py
@@ -1,10 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 
 from lists import views
 
 urlpatterns = [
     path('', views.homepage, name='homepage'),
-    path('lists/new', views.new_list, name='new_list'),
-    path('lists/<int:list_id>/', views.view_list, name='view_list'),
-    path('lists/<int:list_id>/add_item', views.add_item, name='add_item'),
+    path('lists/', include('lists.urls')),
 ]
